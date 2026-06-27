7e143f399e0415d69bf87fbb6444c615c3ef3317
Ayush Kumar <ayushbbu37@gmail.com>
2026-06-27 15:22:12 +0530

    Update fruit list to replace Watermelon with Grapes and add new fruits

--- ListPractice/UpdateList.py
+++ ListPractice/UpdateList.py
@@ -1,7 +1,12 @@
 fruits = ['Apple', 'Mango', 'Papaya', 'Watermelon', 'Delhi', 7, 'Ayush', 'Piyush', 'Ved']
 print(fruits)
 
+
+#Update at an Index
 fruits[3] = 'Grapes'
 print(fruits)
 
+#Update at an Range
+fruits[1:3] = "Ravi", "Raju", "Richa", "Ripu"
+print(fruits)
 
