d5da57aee9cad301da689802e06077a5f75603d8
Ayush Kumar <ayushbbu37@gmail.com>
2026-06-27 23:34:34 +0530

    Fix formatting of list2 initialization in List vs Numpy.py

--- Day-9/List vs Numpy.py
+++ Day-9/List vs Numpy.py
@@ -4,7 +4,7 @@ import time
 x = time.time()
 
 list1 = [i for i in range(100000000)]
-list2 = [i for i in range(100000000,200000000)]
+list2 = [i for i in range(100000000,200000000)] 
 list3 = []
 
 for i in range(len(list1)):
